c3a257d8d83471e9446ae12d34ff29e6604dff37
Keaobaka Matlhola <111046000+kcmatlhola92@users.noreply.github.com>
2023-04-03 14:13:43 +0200

    fix my code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
+    The size of the square must be the first argument
     of the program.
-    change in size = parseInt(process.argv[2], 16)
 */
 
 
